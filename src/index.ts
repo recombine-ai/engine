10a1066d22ecab7f55dae821844c42e6ed5fc760
Roman Bekkiev <rb@rblab.net>
2025-05-16 20:02:22 +0300

    export createAIEngine separately

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import { AIEngine } from './lib/ai'
+
 export { AIEngine } from './lib/ai'
+
+export const createAIEngine = AIEngine.createAIEngine
 
 export { delayFactory, Schedule } from './lib/schedule'
 
-export {Scheduler, Logger} from './lib/interfaces'
+export { Scheduler, Logger } from './lib/interfaces'
 
 export * from './lib/bosun'
